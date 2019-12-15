a03bb405807f2a7e10cbf3463ac07a072384706c
Dragos <dragos199993@gmail.com>
2019-12-15 21:11:16 +0200

    Println formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod print;
+
 fn main() {
-    println!("Hello, world!");
+    print::run();
+    println!("{} Number: {}", 1, "nice");
+    println!("{0} just liked {1} at {1}", "Matthew", "post");
+    println!(
+        "{name} likes {activity}",
+        name = "Christian",
+        activity = "baseball"
+    );
 }
